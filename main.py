ada2dafc8a8014e03537ac788aa4bd7b050bfd75
msuneth <msuneth@gmail.com>
2023-12-30 06:30:01 +0530

    add project titles in two columns

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -14,3 +15,15 @@ with col2:
 
 content2 = """Below are some of the project that I have created so far!!!"""
 st.write(content2)
+
+df = pandas.read_csv("data.csv",sep=";")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
