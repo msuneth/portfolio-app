845d23fa12cf2eef4921ae1a1dfe11d7e34db6d6
msuneth <msuneth@gmail.com>
2023-12-30 06:47:13 +0530

    add project images and source code link

--- main.py
+++ main.py
@@ -16,14 +16,20 @@ with col2:
 content2 = """Below are some of the project that I have created so far!!!"""
 st.write(content2)
 
-df = pandas.read_csv("data.csv",sep=";")
+df = pandas.read_csv("data.csv", sep=";")
 
 col3, col4 = st.columns(2)
 
 with col3:
-    for index,row in df[:10].iterrows():
+    for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index,row in df[10:].iterrows():
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
